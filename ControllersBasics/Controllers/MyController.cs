fe3e57a53524213d7a58fc05b53e8d3d20152928
Roman Tyshchenko <romannt@ukr.net>
2017-09-04 00:27:08 +0300

    Add project files.

---
+++ ControllersBasics/Controllers/MyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllersBasics.Controllers
+{
+    /*
+     * Реализуем свой контроллер путём реализации интерфейса IController.
+     * Для вызова нужно обратиться по пути My в командной строке браузера.
+     */
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var response = requestContext.HttpContext.Response;
+            response.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");
+        }
+    }
+}
